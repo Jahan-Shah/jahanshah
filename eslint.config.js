13838b54b11f7ffe2520d43f7373051d72f3fd15
jahan-shah <shahjahansarfraz3973@gmail.com>
2025-10-22 16:37:45 +0500

    chore: repo init

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+}, {
+  rules: {
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Jahan Shah",
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/x-icon", href: "/favicon-light.ico", media: "(prefers-color-scheme: light)" },
+        { rel: "icon", type: "image/x-icon", href: "/favicon-dark.ico", media: "(prefers-color-scheme: dark)" },
+      ],
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "lenis/nuxt",
+    "@vueuse/nuxt",
+    "motion-v/nuxt",
+    "@nuxtjs/device",
+  ],
+});
